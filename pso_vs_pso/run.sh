1e47020ddbae51b06413549b17d862fd66dde645
Srabon <totalblank@yahoo.com>
2025-11-26 13:47:18 +0600

    ran qq qpso psopso -- at 01:47 PM, Wednesday, 26 Nov 2025

---
+++ pso_vs_pso/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+uv run pso_vs_pso.py
+uv run pso_vs_pso_schemeA.py
+uv run pso_vs_pso_schemeB.py
+uv run pso_vs_pso_schemeC.py
---
+++ q_vs_pso/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+uv run q_vs_pso.py
+uv run q_vs_pso_schemeA.py
+uv run q_vs_pso_schemeB.py
+uv run q_vs_pso_schemeC.py
